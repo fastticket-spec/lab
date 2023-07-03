7e63e8576da54c68edae4ff62a6ba7054ecccba7
Olaide Abdulazeez <dj3plles@gmail.com>
2023-07-03 13:48:10 +0100

    update email footer title

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -25,7 +25,7 @@
 {{-- Footer --}}
 <x-slot:footer>
 <x-mail::footer>
-© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+© {{ date('Y') }} {{ $headerTitle ?? config('app.name') }}. @lang('All rights reserved.')
 </x-mail::footer>
 </x-slot:footer>
 </x-mail::layout>
