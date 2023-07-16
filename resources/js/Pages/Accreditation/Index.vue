0ebd8381264d79fc4dfb2faaa514e014fc420d86
Shafi Abdulrahman <techissued@gmail.com>
2023-07-16 17:50:22 +0300

    Update Index.vue

--- resources/js/Pages/Accreditation/Index.vue
+++ resources/js/Pages/Accreditation/Index.vue
@@ -60,7 +60,9 @@ label {
                 }">
         <div class="row no-gutters">
             <div class="col-12 align-self-center">
-                <div class="bg-div bg-white">
+                <div class="bg-div bg-white" v-if="accessLevel.id == '99a9048c-c682-4e88-9020-0ad13d809c4c' " style="background-color: #3d346a8c!important;">
+                <div class="bg-div bg-white" v-else>
+
                     <div class="text-center">
                         <img class="my-3 text-center img-fluid logo" :src="accessLevel?.event?.event_image_url" alt="">
                     </div>
