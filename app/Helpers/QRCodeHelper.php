5810ac5134cdb8b327fd0d48d4f4a17d49618318
Olaide Abdulazeez <dj3plles@gmail.com>
2023-08-15 12:29:46 +0100

    add qr code to approval mail

---
+++ app/Helpers/QRCodeHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Helpers;
+
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QROptions;
+
+class QRCodeHelper
+{
+    public static function getQRCode(string $text)
+    {
+        $options = new QROptions(
+            [
+                'eccLevel' => QRCode::ECC_L,
+                'outputType' => QRCode::OUTPUT_MARKUP_SVG,
+                'version' => 5,
+            ]
+        );
+
+        return (new QRCode($options))->render($text);
+    }
+}
