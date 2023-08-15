0cb10a08e0cf234f5ca78f6e821dec138b6ad922
Shafi Abdulrahman <techissued@gmail.com>
2023-08-15 16:35:16 +0300

    add mail and qrcode

--- app/Services/AttendeeService.php
+++ app/Services/AttendeeService.php
@@ -263,7 +263,7 @@ class AttendeeService extends BaseRepository
             $settings = optional($attendee->accessLevel)->generalSettings;
 
             Mail::to($attendee->email)
-                ->later(now()->addSeconds(5), new ApprovalMail($settings, $attendee->event->organiser, $attendee->ref));
+                ->later(now()->addSeconds(5), new ApprovalMail($settings, $attendee->event->organiser, $attendee->ref, $attendee->first_name));
         }
     }
 
