4458e4f0ea479bc9170d80d36a33a0bb0f7ac6cf
Olaide Abdulazeez <dj3plles@gmail.com>
2023-11-06 15:56:56 +0100

    fix email issue

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,9 +1,9 @@
-<x-mail::layout bodyBg="{{$preferences['email_bg_color']}}" fontColor="{{$preferences['email_font_color']}}" logoUrl="{{$preferences['email_logo_url']}}" logoWidth="{{$preferences['email_logo_width']}}" logoHeight="{{$preferences['email_logo_height']}}" headerLogo="{{$preferences['email_header_image_url']}}" footerLogo="{{$preferences['email_footer_image_url']}}">
+<x-mail::layout bodyBg="{{$preferences['email_bg_color'] ?? ''}}" fontColor="{{$preferences['email_font_color'] ?? ''}}" logoUrl="{{$preferences['email_logo_url'] ?? ''}}" logoWidth="{{$preferences['email_logo_width'] ?? ''}}" logoHeight="{{$preferences['email_logo_height'] ?? ''}}" headerLogo="{{$preferences['email_header_image_url'] ?? ''}}" footerLogo="{{$preferences['email_footer_image_url'] ?? ''}}">
 {{-- Header --}}
 <x-slot:header>
-<x-mail::header :url="config('app.url')" image="{{$preferences['email_header_image_url']}}">
+<x-mail::header :url="config('app.url')" image="{{$preferences['email_header_image_url'] ?? ''}}">
 {{--    @if($preferences['email_logo_url'])--}}
-{{--    <img src="{{$preferences['email_logo_url']}}" style="width: 200px; height: auto;" alt="">--}}
+ ?? '{{--    <img src="{{$preferences['email_logo_url']}}"  ?? 'style="width: 200px; height: auto;" alt="">--}}
 {{--    @else--}}
 {{--    {{$headerTitle ?? config('app.name')}}--}}
 {{--    @endif--}}
@@ -24,7 +24,7 @@
 
 {{-- Footer --}}
 <x-slot:footer>
-<x-mail::footer image="{{$preferences['email_footer_image_url']}}">
+<x-mail::footer image="{{$preferences['email_footer_image_url'] ?? ''}}">
 © {{ date('Y') }} {{ $headerTitle ?? config('app.name') }}. @lang('All rights reserved.')
 </x-mail::footer>
 </x-slot:footer>
