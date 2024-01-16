67913116fa10809f12f4525d52fe11d12b0df287
Olaide Abdulazeez <dj3plles@gmail.com>
2024-01-16 14:05:53 +0100

    crop images && personal photo field type

---
+++ resources/js/Shared/components/helpers/Cropper.vue
@@ -0,0 +1,29 @@
+<script setup>
+import VueCropper from 'vue-cropperjs';
+import 'cropperjs/dist/cropper.css';
+import {ref} from "vue";
+
+const props = defineProps({
+  imgSrc: Object,
+  onCrop: Function
+})
+
+const cropper = ref('');
+
+const onCropImage = () => {
+  props.onCrop(cropper.value.getCroppedCanvas().toDataURL());
+}
+</script>
+
+<template>
+  <vue-cropper
+      ref="cropper"
+      :aspect-ratio="9 / 9"
+      :src="imgSrc"
+      preview=".preview"
+  />
+
+  <div class="text-right">
+    <b-btn variant="primary" class="mt-3 text-right" @click="onCropImage">Crop</b-btn>
+  </div>
+</template>
